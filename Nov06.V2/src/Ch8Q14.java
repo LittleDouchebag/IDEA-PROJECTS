9ea97d9bda6da1896f67775a6f9eadc1618198c0
LittleDouchebag <LittleDouchebag@users.noreply.github.com>
2018-11-06 08:50:45 +0800

    NONONO

---
+++ Nov06.V2/src/Ch8Q14.java
@@ -0,0 +1,3 @@
+public class Ch8Q14 {
+    
+}
---
+++ Nov06/src/Ch8Q14.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Ch8Q14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello world!");
+    }
+}
